c50d3d4ff1552e9a69973e4e37a35fb56dd8b8a6
erickariasec <erickarias1999@hotmail.com>
2022-09-26 19:00:46 -0500

    Adding Scroll-Up Button

--- projects/landing/scripts.js
+++ projects/landing/scripts.js
@@ -5,17 +5,17 @@ $(document).ready(function () {
         } else {
             $(".navbar").removeClass("sticky");
         }
-        // if (this.scrollY > 500) {
-        //     $(".scroll-up-btn").addClass("show");
-        // } else {
-        //     $(".scroll-up-btn").removeClass("show");
-        // }
+        if (this.scrollY > 50) {
+            $(".scroll-up-btn").addClass("show");
+        } else {
+            $(".scroll-up-btn").removeClass("show");
+        }
     });
 
     // slide-up script
-    // $(".scroll-up-btn").click(function () {
-    //     $("html").animate({ scrollTop: 0 });
-    // });
+    $(".scroll-up-btn").click(function () {
+        $("html").animate({ scrollTop: 0 });
+    });
 
     // toggle menu/navbar script
     $(".menu-btn").click(function () {
