dd48461b0a4aa1dd01104eaf88488c53e59f82f8
Gabriel Vazquez <gavf.vazquez@gmail.com>
2023-02-09 20:54:14 -0300

    Make stories, add header, description and carousel

--- projects/uao-ukraine-assistance-organization/scripts.js
+++ projects/uao-ukraine-assistance-organization/scripts.js
@@ -1,15 +1,20 @@
 $(document).ready(function () {
-    $(window).scroll(function () {
-        if (this.scrollY > 0) {
-            $(".navbar").addClass("sticky");
-        } else {
-            $(".navbar").removeClass("sticky");
-        }
-    });
+  $(window).scroll(function () {
+    if (this.scrollY > 0) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
+  });
 
-    // toggle menu/navbar script
-    $(".menu-btn").click(function () {
-        $(".navbar .menu").toggleClass("active");
-        $(".menu-btn i").toggleClass("active");
-    });
+  // toggle menu/navbar script
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
 });
+
+// Section carousel
+var mySwiper = new Swiper('.mySwiper', {
+    initialSlide: 1, // iniciar en el segundo slide
+    });
