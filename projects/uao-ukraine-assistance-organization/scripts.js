60ba376efaf8bdc346e8587e55bc710875338259
erickariasec <erickarias1999@hotmail.com>
2023-03-21 18:32:00 -0500

    Refactor styling of toggle swtich at navbar

--- projects/uao-ukraine-assistance-organization/scripts.js
+++ projects/uao-ukraine-assistance-organization/scripts.js
@@ -47,3 +47,20 @@ $(document).ready(function(){
         $('#variante').text('Difference.');
     }, 9000);
 });
+
+
+$(document).ready(function () {
+    $("#color_mode").on("change", function () {
+        colorModePreview(this);
+    });
+});
+
+function colorModePreview(ele) {
+    if ($(ele).prop("checked") == true) {
+        $("body").addClass("dark-preview");
+        $("body").removeClass("white-preview");
+    } else if ($(ele).prop("checked") == false) {
+        $("body").addClass("white-preview");
+        $("body").removeClass("dark-preview");
+    }
+}
