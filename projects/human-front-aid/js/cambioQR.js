7200dc06a0ca6dc9ff26f3ab2b6ecb1938ce74f5
Diego N. Cabrera <87397817+1Hagi@users.noreply.github.com>
2023-10-18 20:34:58 -0300

    donate - buttons and qr tablet fixed.

--- projects/human-front-aid/js/cambioQR.js
+++ projects/human-front-aid/js/cambioQR.js
@@ -1,5 +1,11 @@
 function cambiarQR(rutaImagen){
     var imagen = document.getElementsByClassName("imagen-qr");
     imagen[0].src = rutaImagen;
-}
 
+
+    /* Efecto de scroll para la vista en Tablet */
+    if (screen.width >= 481 && screen.width <= 992) {
+        window.scroll(0, 450);
+    }
+    
+}
